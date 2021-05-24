855b0abb2544d470ccdd62e078fa34170ee75a9b
jackjack210 <67781631+jackjack210@users.noreply.github.com>
2021-05-24 13:45:43 +0530

    Update Login.js

--- reg-log/client/src/components/Login.js
+++ reg-log/client/src/components/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
     const loginUser = async (e) =>{
         e.preventDefault();
 
-        const res = await fetch('/registration', {
+        const res = await fetch('/signin', {
             method:'POST',
             headers: {
                 "Content-Type" : "application/json"
